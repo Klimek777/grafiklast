74e0d2b94cf75e0ea0c732f919877a8d477da71e
Dawid Klimowicz <dawidkl777@gmail.com>
2023-04-05 20:22:41 +0200

    Initial commit

---
+++ src/main/java/com/example/grafiklast/GrafiklastApplication.java
@@ -0,0 +1,13 @@
+package com.example.grafiklast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrafiklastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrafiklastApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/grafiklast/GrafiklastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grafiklast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrafiklastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
